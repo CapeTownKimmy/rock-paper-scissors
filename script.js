927552cd4fb2cca77bf3064ba75ebcb020647730
Kim Parkes <122791435+CapeTownKimmy@users.noreply.github.com>
2023-03-02 14:41:35 +0000

    Add pseudocode structure to .js file

---
+++ script.js
@@ -0,0 +1,51 @@
+// Assign integers to the rock, paper, scissors variables.
+
+const rock = 1
+const paper = 2
+const scissors = 3
+
+
+// Computer choses random option of rock, paper or scissors.
+
+
+// Prompt user to select rock, paper or scissors.
+// Adjust users Input to LOWER CASE.
+
+
+
+// if the users choice and computers choice are EQUAL then return a TIE message to the console.
+
+// ROCK: If the user choice is ROCK and computer choice is PAPER then return a COMPUTER WINS message to the console.
+// Add 1 to COMPUTERS score.
+
+// ROCK: If the users choice is ROCK and computer choice is SCISSORS then return a USER WINS message to the console.
+// Add 1 to the USERS score.
+
+
+
+// PAPER: If the user choice is PAPER and computer choice is ROCK then return a USER WINS message to the console.
+// Add 1 to USERS score.
+
+// PAPER: If the user choice is PAPER and computer choice is SCISSORS then return a COMPUTER WINS message to the console.
+// Add 1 to COMPUTERS score.
+
+
+
+// SCISSORS: If the user choice is SCISSORS and computer choice is ROCK then return a COMPUTER WINS message to the console.
+// Add 1 to COMPUTER score.
+
+// SCISSORS: If the user choice is SCISSORS and computer choice is PAPER then return a USER WINS message to the console.
+// Add 1 to USERS score.
+
+
+// DEFAULT: If user inputs doesnt match rock, paper, or scissors then return an ERROR message to the console.
+
+
+// GAME lasts for 5 ROUNDS.
+// Compare scores and assign a winner.
+
+
+// RESET to play again.
+
+
+
