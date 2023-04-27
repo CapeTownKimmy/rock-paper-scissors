9b0ba940d5629afb44bf6d9edb882ab446bc14b8
Kim Parkes <122791435+CapeTownKimmy@users.noreply.github.com>
2023-04-27 14:06:16 +0100

    Edit Js and Html to work with new UI

--- script.js
+++ script.js
@@ -1,45 +1,75 @@
 //Display elements: choices and score.
 
-const playerChoiceDisp = document.getElementById('playerChoice')
-const computerChoiceDisp = document.getElementById('computerChoice')
-const resultDisp = document.getElementById('winner')
 const possibleChoices = document.querySelectorAll('button')
-const playerScoreDisp = document.getElementById('playerScore')
-const computerScoreDisp = document.getElementById('computerScore')
-const gameWinner = document.getElementById('gameWinner')
+const playerChoiceDisp = document.getElementsByClassName('playerIcon')
+const computerChoiceDisp = document.getElementsByClassName('computerIcon')
+const resultDisp = document.getElementById('winner')
+const playerScoreDisp = document.getElementsByClassName('playerScoreBox')
+const computerScoreDisp = document.getElementsByClassName('computerScoreBox')
+const winnerModal = document.getElementById('winnerModal')
+
 let playerChoice
 let computerSelection
+let playerElement
+let compElement
 let result
 let playerScore = 0
 let computerScore = 0
 
-// User Choice
+// User Choice Button clicks//
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) =>{
     playerChoice = e.target.id
-    playerChoiceDisp.innerHTML = playerChoice
+    showPlayerIcon()
     generateCompChoice()
-    getResult()
-    gameOver()
+    // getResult()
+    // gameOver()
 }))
+
+
+//Display choice icons in display box//
+
+function showPlayerIcon() {
+
+    switch (playerChoice) {
+        case 'rock':
+            playerElement = document.getElementById('playerRockIcon').style.visibility = 'visible';
+            break;
+        case 'paper':
+            playerElement = document.getElementById('playerPaperIcon').style.visibility = 'visible';
+            break;
+        case 'scissors':
+            playerElement = document.getElementById('playerScissorsIcon').style.visibility = 'visible';
+            break;
+    }
+    playerChoiceDisp.innerHTML = playerElement;
+    console.log(playerChoice);
+}
+
+
+// Generating random comp selection//
 
 function generateCompChoice(){
 
     computerSelection = Math.floor(Math.random() * 3) + 1;
 
-switch (computerSelection) {
-    case 1: 
-        computerSelection = 'rock';
-        break;
-    case 2:
-        computerSelection = 'paper';
-        break;
-    case 3:
-        computerSelection = 'scissors';
-        break;
-}
+        switch (computerSelection) {
+            case 1: 
+                computerSelection = 'rock';
+                compElement = document.getElementById('compRockIcon').style.visibility = 'visible';;
+                break;
+            case 2:
+                computerSelection = 'paper';
+                compElement = document.getElementById('compPaperIcon').style.visibility = 'visible';;
+                break;
+            case 3:
+                computerSelection = 'scissors';
+                compElement = document.getElementById('compScissorsIcon').style.visibility = 'visible';
+                break;
+        }
 
-computerChoiceDisp.innerHTML = computerSelection
+    computerChoiceDisp.innerHTML = compElement
+    console.log(computerSelection);
 }
 
 //Round
